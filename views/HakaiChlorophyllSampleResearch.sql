dd9d98318e4ecbbff7b6cbfec1d819c4d652d1e1
Jessy Barrette <jessy.barrette@hakai.org>
2021-06-01 14:53:03 -0700

    Add 2um filter size to Chlorophyll view

--- views/HakaiChlorophyllSampleResearch.sql
+++ views/HakaiChlorophyllSampleResearch.sql
@@ -15,11 +15,13 @@ SELECT "work_area",
     -- There are a few cases where records have multiple nil
     (array_remove(array_agg(phaeo_20um), Null)) [1]::NUMERIC phaeo_20um,
     (array_remove(array_agg(phaeo_3um), Null)) [1]::NUMERIC phaeo_3um,
+    (array_remove(array_agg(phaeo_2um), Null)) [1]::NUMERIC phaeo_2um,
     (array_remove(array_agg(phaeo_gf_f), Null)) [1]::NUMERIC phaeo_gf_f,
     (array_remove(array_agg(phaeo_bulk_gf_f), Null)) [1]::NUMERIC phaeo_bulk_gf_f,
     -- chla
     (array_remove(array_agg(chla_20um), Null)) [1]::NUMERIC chla_20um,
     (array_remove(array_agg(chla_3um), Null)) [1]::NUMERIC chla_3um,
+    (array_remove(array_agg(chla_2um), Null)) [1]::NUMERIC chla_2um,
     (array_remove(array_agg(chla_gf_f), Null)) [1]::NUMERIC chla_gf_f,
     (array_remove(array_agg(chla_bulk_gf_f), Null)) [1]::NUMERIC chla_bulk_gf_f
 FROM (
@@ -37,8 +39,14 @@ FROM (
             ) phaeo_3um,
             (
                 CASE
+                    WHEN filter_type = '2um' THEN phaeo
+                END
+            ) phaeo_2um,
+            (
+                CASE
                     WHEN filter_type = 'GF/F' THEN phaeo
                 END
+            
             ) phaeo_gf_f,
             (
                 CASE
@@ -56,6 +64,11 @@ FROM (
                     WHEN filter_type = '3um' THEN chla
                 END
             ) chla_3um,
+            (
+                CASE
+                    WHEN filter_type = '2um' THEN chla
+                END
+            ) chla_2um,
             (
                 CASE
                     WHEN filter_type = 'GF/F' THEN chla
