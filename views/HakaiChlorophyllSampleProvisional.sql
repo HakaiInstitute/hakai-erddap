9bee168d5dfcbef35845b0e25166cee210988900
Jessy Barrette <jessy.barrette@hakai.org>
2021-06-04 15:42:59 -0700

    Fix typo on CHlorophyll Provisional dataset

--- views/HakaiChlorophyllSampleProvisional.sql
+++ views/HakaiChlorophyllSampleProvisional.sql
@@ -5,7 +5,7 @@ SELECT
 (case when subquery2.chla_3um is not null then subquery2.chla_gf_f else null end) chla_3um_gf_f,
 (case when subquery2.chla_2um is not null then subquery2.chla_gf_f else null end) chla_2um_gf_f,
 (case when subquery2.chla_2um is not null then subquery2.chla_gf_f_flag else null end) chla_2um_gf_f_flag,
-(case when subquery2.chla_2um is not null then subquery2.chla_gf_f_flag else null end) chla_2um_gf_f_flag
+(case when subquery2.chla_3um is not null then subquery2.chla_gf_f_flag else null end) chla_3um_gf_f_flag
  from (
 SELECT "work_area",
     "organization",
