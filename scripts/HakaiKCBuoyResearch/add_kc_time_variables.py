9bd9867463250044b95c04524a94526f43894b46
Jessy Barrette <30420025+JessyBarrette@users.noreply.github.com>
2023-02-08 14:53:06 -0500

    fix kc buoy script to use argparse

--- scripts/HakaiKCBuoyResearch/add_kc_time_variables.py
+++ scripts/HakaiKCBuoyResearch/add_kc_time_variables.py
@@ -17,14 +17,13 @@ def convert_kc_buoy(input_path, output_path=None):
 
 if __name__ == "__main__":
 
-    # parser = argparse.ArgumentParser(prog='add_kc_time_variable',
-    #                                  description="OA CSV format has time and date seperated this just regroup them into a time variable.")
-    # parser.add_argument(
-    #     '-i', type=str, dest="input_file_csv",
-    #     help="CSV file to convert")
-    #
-    # args = parser.parse_args()
-    # convert_kc_buoy(args.input_file_csv)
-    convert_kc_buoy(
-        r"C:\Users\jessy\Documents\repositories\erddap-basic\datasets\HakaiKCBuoyResearch\187F20180501_May2018_Oct2020.csv"
+    parser = argparse.ArgumentParser(
+        prog="add_kc_time_variable",
+        description="OA CSV format has time and date seperated this just regroup them into a time variable.",
     )
+    parser.add_argument(
+        "-i", type=str, dest="input_file_csv", help="CSV file to convert"
+    )
+
+    args = parser.parse_args()
+    convert_kc_buoy(args.input_file_csv)
