c79dda2b124360c867ac815b00908d878fc79ca1
Jessy Barrette <30420025+JessyBarrette@users.noreply.github.com>
2023-11-23 16:27:02 -0500

    bring back the original bash script

--- init.d/replace_erddap_dataset_secrets.sh
+++ init.d/replace_erddap_dataset_secrets.sh
@@ -1,18 +1,8 @@
 #!/bin/bash
-
-# Path to the datasets.xml file
-file_path="/usr/local/tomcat/content/erddap/datasets.xml"
-
-# Get a list of all environment variables starting with "ERDDAP_SECRET_"
-secret_vars=$(env | grep '^ERDDAP_SECRET_' | awk -F= '{print $1}')
-
-# Loop through each secret variable and replace its occurrence in the file
-for var in $secret_vars; do
-    # Get the corresponding value of the environment variable
-    value=${!var}
-    
-    # Replace the placeholder string with the environment variable's value
-    sed -i "s|__${var}__|${value}|g" $file_path
-done
-
-echo "Environment variables replaced in $file_path"
+# Replace secrets in dataset.xml by their corresponding 
+#  environment variables ERDDAP_SECRET_(.*)
+while read -r e; do
+  k=$(echo "$e" | cut -d= -f1);
+  v=$(echo "$e" | cut -d= -f2-);
+  sed -i "s@$k@$v@g" /usr/local/tomcat/content/erddap/datasets.xml
+done < <(env | grep -oP '(?<=^ERDDAP_SECRET_).*')
