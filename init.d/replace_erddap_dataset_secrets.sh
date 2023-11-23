ba63b8545238b6e4ef46265ccffc7a8754968214
Jessy Barrette <30420025+JessyBarrette@users.noreply.github.com>
2023-11-23 16:08:28 -0500

    add back spaces in script

--- init.d/replace_erddap_dataset_secrets.sh
+++ init.d/replace_erddap_dataset_secrets.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 # Path to the datasets.xml file
 file_path="/usr/local/tomcat/content/erddap/datasets.xml"
+
 # Get a list of all environment variables starting with "ERDDAP_SECRET_"
 secret_vars=$(env | grep '^ERDDAP_SECRET_' | awk -F= '{print $1}')
+
 # Loop through each secret variable and replace its occurrence in the file
 for var in $secret_vars; do
     # Get the corresponding value of the environment variable
@@ -11,4 +13,5 @@ for var in $secret_vars; do
     # Replace the placeholder string with the environment variable's value
     sed -i "s|__${var}__|${value}|g" $file_path
 done
+
 echo "Environment variables replaced in $file_path"
