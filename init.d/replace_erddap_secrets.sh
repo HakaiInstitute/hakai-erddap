bb6b70fd3b5c2dfe3b9a1c72ec3cdbd3bce6552e
Jessy Barrette <30420025+JessyBarrette@users.noreply.github.com>
2023-11-28 16:17:00 -0500

    add replace- secret script tested

--- init.d/replace_erddap_secrets.sh
+++ init.d/replace_erddap_secrets.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 for var in $(env); do
-    if echo $var | grep -q 'GH_.*$'; then
-        k=$(echo "$var" | cut -d "=" -f1);
+    if echo $var | grep -q 'ERDDAP_SECRET_.*$'; then
+        k=$(echo "$var" | cut -d "=" -f1 | sed s/ERDDAP_SECRET_//);
         v=$(echo "$var" | cut -d "=" -f2-);
-        echo "k=$k v=$v"
+        echo "replace secrets=$k"
         sed -i "s@$k@$v@g" /usr/local/tomcat/content/erddap/datasets.xml
     fi
 done
