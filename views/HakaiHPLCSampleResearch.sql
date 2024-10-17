2f5a820bc8703ce96a1f235ba0046c730c56e0be
Matthew Foster <matthew.foster@hakai.org>
2024-10-17 09:18:57 -0700

    Revert "changes on hecate, not sure if they are ok but can't merge git changes with these hanging around."
    This reverts commit 14e56ceda3bb24ffcc039aea887328e01891f989.

--- views/HakaiHPLCSampleResearch.sql
+++ views/HakaiHPLCSampleResearch.sql
@@ -13,7 +13,8 @@ join eims.output_sites y
 on x.site_id = y.name and x.work_area = y.work_area
 WHERE
     x.organization = 'HAKAI'
-    and x.row_flag = 'Results'
-    -- AND quality_level IN ('Principal Investigator', 'Technicianmr')
+    AND x.row_flag = 'Results'
+    AND quality_level IN ('Principal Investigator', 'Technicianmr')
+    AND x.hplc.all_chl_a_flag IN ('AV')
     -- AND site_id IN ('QU39')
 ;
