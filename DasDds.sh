cea662b2a287e672620f6c7f21a0feade0bfee28
Matthew Foster <matthew.foster@hakai.org>
2025-03-11 11:52:11 -0700

    maybe this env export will work

--- DasDds.sh
+++ DasDds.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 #[[ -f ./.env ]] && export $(grep -v '^#' .env | xargs)
 #[[ -f ./.env ]] && grep -v '^#' .env | tr '\n' '\0' | xargs -0 | export
+# env $(cat .env)
 IFS='
 '
-env $(cat .env)
+export $(grep -v '^#' .env | xargs -d '\n')
 docker exec -it "${CONTAINER_NAME:-erddap}" bash -c "cd webapps/erddap/WEB-INF/ && bash DasDds.sh $*" \
   
