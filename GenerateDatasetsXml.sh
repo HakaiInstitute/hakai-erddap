c7e884efc31bf8de11e9cff836b24d725893f078
Matthew Foster <matthew.foster@hakai.org>
2025-06-02 11:36:14 -0700

    update dataset xml (#215)
    * deploy to erddap.hakai.app development erddap?
    
    * reorder environment variable
    
    * Sync ERDDAP metadata from metadata form file repository
    
    Signed-off-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * return to using hakai-erddap repo install
    
    * add log level to syn action
    
    * remove duplicate mount
    
    * update readme and sample.env
    
    * add initial orford bouy all metadata is still copy og kc bout
    
    * maybe this env export will work
    
    * Revert "Merge branch 'master' into development"
    
    This reverts commit c05bdc98466e17054dfa17a903890a88505e9741, reversing
    changes made to cea662b2a287e672620f6c7f21a0feade0bfee28.
    
    merge main into dev
    
    * remove trailing whitespace
    
    * Sync ERDDAP metadata from metadata form file repository (#214)
    
    Signed-off-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: fostermh <6304992+fostermh@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: fostermh <6304992+fostermh@users.noreply.github.com>
    Co-authored-by: hakai-it <44271393+hakai-it@users.noreply.github.com>

--- GenerateDatasetsXml.sh
+++ GenerateDatasetsXml.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-
-if [ -f .env ]; then
-    source .env
-fi
+[[ -f ./.env ]] && grep -v '^#' .env | tr '\n' '\0' | xargs -0 | export
 docker exec -it "${CONTAINER_NAME:-erddap}" bash -c "cd webapps/erddap/WEB-INF/ && bash GenerateDatasetsXml.sh -verbose $*" \
